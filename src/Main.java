8a77b25f8ae02fd2d8dc47367adb6b36bd5fc305
Thunder-K <rlaxo7244@gmail.com>
2024-06-29 20:01:46 +0900

    init commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("공배수");
+        System.out.println(CodeTest.solution1(60,2,3));
+
+        System.out.println("카운트 업");
+        System.out.println(Arrays.toString(CodeTest.solution2(3, 10)));
+
+        System.out.println("수 조작하기 1");
+        System.out.println(CodeTest.solution3(0, "wsdawsdassw"));
+
+        System.out.println("마지막 두 원소");
+        System.out.println(Arrays.toString(CodeTest.solution4(new int[]{2, 1, 6})));
+
+
+    }
+
+}
