ae1b189a24d83bf93a6d184c20eece06260886c4
Thunder-K <rlaxo7244@gmail.com>
2024-07-07 18:26:57 +0900

    CodeTestLv0 으로 통합, solution12 추가

---
+++ src/CodeTestLv0.java
@@ -0,0 +1,176 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CodeTestLv0 {
+
+    public static int solution1(int number, int n, int m) {
+
+        int nCheck = number % n == 0 ? 1 : 0;
+        int mCheck = number % m == 0 ? 1 : 0;
+
+        return nCheck*mCheck;
+    }
+
+    public static int[] solution2(int start_num, int end_num) {
+        int[] answer = {};
+
+        List<Integer> intList = new ArrayList<>();
+
+        for(int i = start_num; i <= end_num; i++) {
+
+            intList.add(i);
+
+        }
+        answer = intList.stream().mapToInt(Integer::intValue).toArray();
+
+        return answer;
+    }
+
+    public static int solution3(int n, String control) {
+        int answer = 0;
+
+        char[] arr = control.toCharArray();
+
+        for (char c : arr) {
+            if (c == 'w')
+                n += 1;
+            else if (c == 's')
+                n -= 1;
+            else if (c == 'd')
+                n += 10;
+            else if (c == 'a')
+                n -= 10;
+        }
+
+        answer = n;
+
+        return answer;
+    }
+
+    public static int[] solution4(int[] num_list) {
+        int[] answer = {};
+
+        int lastIndex = num_list.length - 1;
+        int lastValue = 0;
+        int[] add_num_list = new int[lastIndex + 2];
+
+        if(num_list[lastIndex] > num_list[lastIndex-1])
+            lastValue = num_list[lastIndex] - num_list[lastIndex-1];
+        else
+            lastValue = num_list[lastIndex] * 2;
+
+        int arr_num_list_index = 0;
+
+        for(int i : num_list) {
+            add_num_list[arr_num_list_index] = i;
+            arr_num_list_index++;
+        }
+        add_num_list[add_num_list.length-1] = lastValue;
+
+        answer = add_num_list;
+
+        return answer;
+    }
+
+    public static int solution5(int[] num_list) {
+        int answer = 0;
+
+        String oddStr = Arrays.stream(num_list)
+                .filter(x -> x % 2 != 0)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining());
+
+        String evenStr = Arrays.stream(num_list)
+                .filter(x -> x % 2 == 0)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining());
+
+        int oddNumber = oddStr.isEmpty() ? 0 : Integer.parseInt(oddStr);
+        int evenNumber = evenStr.isEmpty() ? 0 : Integer.parseInt(evenStr);
+
+        answer = oddNumber + evenNumber;
+
+        return answer;
+    }
+
+    public static int solution6(int[] num_list) {
+        int answer = 0;
+
+        int allDiv = Arrays.stream(num_list).reduce(1,(a,b) -> a*b);
+
+        System.out.println("allDiv >> " + allDiv);
+
+        int allPlus = Arrays.stream(num_list).sum();
+
+        System.out.println("allPlus >> " + allDiv);
+
+        answer = allDiv > (allPlus*allPlus) ? 0 : 1;
+
+        return answer;
+    }
+
+    public static int solution7(int a, int b, boolean flag) {
+        int answer = 0;
+
+
+
+        return (flag ? a+b : a-b);
+    }
+
+    public static int solution8(int n) {
+        int answer = 0;
+
+        if(n % 2 == 0) {
+
+            for(int i = 2; i <= n; i+=2) {
+                answer += (int) Math.pow(i, 2);
+            }
+
+        } else {
+
+            for(int i = 1; i <= n; i+=2) {
+                answer += i;
+            }
+
+        }
+
+
+
+        return answer;
+    }
+
+    public static int[] solution9(int[] arr, int k) {
+        int[] answer = {};
+
+        answer = Arrays.stream(arr).map(i -> k % 2 != 0 ? i * k : i + k).toArray();
+
+        return answer;
+    }
+
+    public static int solution10(int[] num_list, int n) {
+        int answer = 0;
+
+        answer = Arrays.stream(num_list).anyMatch(i -> i == n) ? 1 : 0;
+
+        return answer;
+    }
+
+    public static int solution11(String str1, String str2) {
+        int answer = 0;
+
+        answer = (str2.contains(str1) ? 1 : 0);
+
+        return answer;
+    }
+
+    public static int solution12(String my_string, String target) {
+        int answer = 0;
+
+        answer = (my_string.contains(target) ? 1 : 0);
+
+        return answer;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -5,37 +5,40 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("공배수");
-        System.out.println(CodeTest.solution(60,2,3));
+        System.out.println(CodeTestLv0.solution1(60,2,3));
 
         System.out.println("카운트 업");
-        System.out.println(Arrays.toString(CodeTest2.solution(3, 10)));
+        System.out.println(Arrays.toString(CodeTestLv0.solution2(3, 10)));
 
         System.out.println("수 조작하기 1");
-        System.out.println(CodeTest3.solution(0, "wsdawsdassw"));
+        System.out.println(CodeTestLv0.solution3(0, "wsdawsdassw"));
 
         System.out.println("마지막 두 원소");
-        System.out.println(Arrays.toString(CodeTest4.solution(new int[]{2, 1, 6})));
+        System.out.println(Arrays.toString(CodeTestLv0.solution4(new int[]{2, 1, 6})));
 
         System.out.println("이어 붙인 수");
-        System.out.println(CodeTest5.solution(new int[]{3,4,5,2,1}));
+        System.out.println(CodeTestLv0.solution5(new int[]{3,4,5,2,1}));
 
         System.out.println("원소들의 곱과 합");
-        System.out.println(CodeTest6.solution(new int[]{5, 7, 8, 3}));
+        System.out.println(CodeTestLv0.solution6(new int[]{5, 7, 8, 3}));
 
         System.out.println("flag에 따라 다른 값 반환하기");
-        System.out.println(CodeTest7.solution(-4, 7, true));
+        System.out.println(CodeTestLv0.solution7(-4, 7, true));
 
         System.out.println("홀짝에 따라 다른 값 반환하기");
-        System.out.println(CodeTest8.solution(7));
+        System.out.println(CodeTestLv0.solution8(7));
 
         System.out.println("조건에 맞게 수열 변환하기 3");
-        System.out.println(Arrays.toString(CodeTest9.solution(new int[]{1, 2, 3, 100, 99, 98}, 3)));
+        System.out.println(Arrays.toString(CodeTestLv0.solution9(new int[]{1, 2, 3, 100, 99, 98}, 3)));
 
         System.out.println("정수 찾기");
-        System.out.println(CodeTest10.solution(new int[]{1, 2, 3, 4, 5}, 3));
+        System.out.println(CodeTestLv0.solution10(new int[]{1, 2, 3, 4, 5}, 3));
 
         System.out.println("부분 문자열");
-        System.out.println(CodeTest11.solution("abc", "aabcc"));
+        System.out.println(CodeTestLv0.solution11("abc", "aabcc"));
+
+        System.out.println("부분 문자열인지 확인하기");
+        System.out.println(CodeTestLv0.solution12("banana", "ana"));
 
     }
 
